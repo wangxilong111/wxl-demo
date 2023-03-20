0ecad059f37a5ab85d3cdcb687ade4d59f44ac18
wxl <2829336975@qq.com>
2023-03-20 16:55:58 +0800

    Initial commit

---
+++ ServiceDemo/src/main/java/com/xmy/ServiceApplication.java
@@ -0,0 +1,16 @@
+package com.xmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Admin
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class,args);
+    }
+}
---
+++ Zuul_Demo/src/main/java/com/xmy/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.xmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Admin
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
