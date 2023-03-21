bb1746076eef08c9d05e59c904d95a8112d17c8b
wxl <2829336975@qq.com>
2023-03-21 15:19:26 +0800

    变更

---
+++ demo-model/src/main/java/com/xmy/system/domain/UserBasicInfo.java
@@ -0,0 +1,14 @@
+package com.xmy.system.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * 用户基本信息 user_basic_info
+ * @author WXL
+ *  @date 2023-03-21
+ */
+@Data
+@TableName
+public class UserBasicInfo {
+}
